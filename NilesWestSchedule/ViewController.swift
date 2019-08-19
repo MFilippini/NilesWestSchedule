1c59b61fb06aa3f88316d60fcd18c83bc20e2199
Michael Filippini <michaelfilippini20@gmail.com>
2019-08-19 14:28:36 -0500

    pods and db setup

--- NilesWestSchedule/ViewController.swift
+++ NilesWestSchedule/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
+var todaysDate = ""
 class ViewController: UIViewController {
-
+    
+    
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        setColors()
+        loadSchedule()
     }
-
-
+    
+    func setColors(){
+        
+    }
+    
+    func loadSchedule(){
+        let cal = Calendar.current
+        let components = cal.dateComponents([ .month, .day, .weekday], from: Date())
+        print("date\(components)")
+        let currentMonth = components.month ?? 0
+        let currentDay = components.day ?? 0
+        print("\(currentMonth)-\(currentDay)")
+        
+        ref.child("schedules").child("regular").observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            print(value)
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
