e48bc94df42fc7944b078df709e1792776dabbb6
Michael Filippini <michaelfilippini20@gmail.com>
2019-08-21 23:08:01 -0500

    can check for special schedule and use that schedule

--- NilesWestSchedule/ViewController.swift
+++ NilesWestSchedule/ViewController.swift
@@ -39,44 +39,55 @@ class ViewController: UIViewController {
         print("date\(components)")
         let currentMonth = components.month ?? 0
         let currentDay = components.day ?? 0
-        print("\(currentMonth)-\(currentDay)")
         
-        ref.child("schedules").child("regular").observeSingleEvent(of: .value, with: { (snapshot) in
-            let scheduleDict = snapshot.value as? NSDictionary ?? [:]
+        ref.child("dates").child("\(currentMonth)-\(currentDay)").observeSingleEvent(of: .value, with: { (snapshot) in
+            let date = snapshot.value as? NSDictionary ?? [:]
+            var schedule = "regular"
             
-            for (key,value) in scheduleDict{
-                let key = key as? String ?? "noString"
-                if(key != "name"){
-                    let value = value as? NSDictionary ?? [:]
-                    let startTime = value["start"] as? Double  ?? 0
-                    let endTime = value["end"] as? Double  ?? 0
-                    
-                    var realStartTime = startTime
-                    var realEndTime = endTime
-                    if(startTime<6.3){ //times 1:00 - 6:29 are assumed to be PM 6:30 - 12:59 are AM
-                        realStartTime = startTime + 12
-                        realEndTime = endTime + 12
+            if date != [:]{
+                schedule = date["schedule"] as? String ?? "none"
+            }
+            
+            self.ref.child("schedules").child(schedule).observeSingleEvent(of: .value, with: { (snapshot) in
+                let scheduleDict = snapshot.value as? NSDictionary ?? [:]
+                
+                for (key,value) in scheduleDict{
+                    let key = key as? String ?? "noString"
+                    if(key != "name"){
+                        let value = value as? NSDictionary ?? [:]
+                        let startTime = value["start"] as? Double  ?? 0
+                        let endTime = value["end"] as? Double  ?? 0
+                        
+                        var realStartTime = startTime
+                        var realEndTime = endTime
+                        if(startTime<6.3){ //times 1:00 - 6:29 are assumed to be PM 6:30 - 12:59 are AM
+                            realStartTime = startTime + 12
+                            realEndTime = endTime + 12
+                        }
+                        
+                        dailySchedule.append([key,startTime,endTime,realStartTime,realEndTime])
                     }
-                    
-                    dailySchedule.append([key,startTime,endTime,realStartTime,realEndTime])
                 }
-            }
-            
-            dailySchedule = dailySchedule.sorted{($0[3] as! Double) < ($1[3]as! Double)}
-            
-            let formater = NumberFormatter()
-            formater.decimalSeparator = ":"
-            formater.maximumFractionDigits = 2
-            formater.minimumFractionDigits = 2
-            formater.roundingMode = .halfUp
-            
+                
+                dailySchedule = dailySchedule.sorted{($0[3] as! Double) < ($1[3]as! Double)}
+                
+                let formater = NumberFormatter()
+                formater.decimalSeparator = ":"
+                formater.maximumFractionDigits = 2
+                formater.minimumFractionDigits = 2
+                formater.roundingMode = .halfUp
+                
 
-            for i in 0..<dailySchedule.endIndex {
-                dailySchedule[i][1] = formater.string(from: NSNumber(value: dailySchedule[i][1] as? Double ?? 0)) ?? "0"
-                dailySchedule[i][2] = formater.string(from: NSNumber(value: dailySchedule[i][2] as? Double ?? 0)) ?? "0"
-            }
-            
-            print(dailySchedule)
+                for i in 0..<dailySchedule.endIndex {
+                    dailySchedule[i][1] = formater.string(from: NSNumber(value: dailySchedule[i][1] as? Double ?? 0)) ?? "0"
+                    dailySchedule[i][2] = formater.string(from: NSNumber(value: dailySchedule[i][2] as? Double ?? 0)) ?? "0"
+                }
+                
+                print(dailySchedule)
+                
+            }) { (error) in
+                print(error.localizedDescription)
+                }
             
         }) { (error) in
             print(error.localizedDescription)
